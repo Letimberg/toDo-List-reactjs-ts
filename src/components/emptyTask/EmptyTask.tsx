da9235ca41f272b5230577e8c45eea9176f3e401
letimberg.cavalcante <letimberg.cavalcante@gmail.com>
2023-10-12 17:55:28 -0300

    Construindo layout

---
+++ src/components/emptyTask/EmptyTask.tsx
@@ -0,0 +1,12 @@
+import styles from "./EmptyTask.module.css"
+import clipboard from "../assets/clipboard.svg"
+
+export function EmptyTasks() {
+    return (
+        <div className={styles.todo}>
+            <img src={clipboard} />
+            <strong>Você ainda não tem tarefas cadastradas</strong>
+            <p>crie tarefas e organize seus itens a fazer</p>
+        </div>
+    )
+}
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,10 @@
+import styles from "./Header.module.css"
+import headerLogo from "../../assets/header-logo.svg"
+
+export function Header() {
+    return (
+        <header className={styles.header}>
+            <img src={headerLogo} alt="" />
+        </header>
+    )
+}
